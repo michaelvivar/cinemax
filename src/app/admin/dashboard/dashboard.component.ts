d82bfbafe3f04e2da3dcc3ffbaf6b5425993057e
Michael Vivar <michaelvictorvivar@gmail.com>
2018-06-20 13:42:36 +0800

    display data

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from '../../models/movie.model';
+import { BaseComponent } from '../../utils/base.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styles: []
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { super() }
+
+  movies: Movie[];
 
   ngOnInit() {
+    this.movies = (<any[]>this.route.snapshot.data['movies']).sort(this.sortBy('-date'));
   }
-
 }
