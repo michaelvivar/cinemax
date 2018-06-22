2afb89b89d7b54457cc184b6d806a7040f249c23
Michael Vivar <michaelvivar@michaels-MacBook-Pro.local>
2018-06-22 09:12:32 +0800

    added methods

--- src/app/services/theater.service.ts
+++ src/app/services/theater.service.ts
@@ -3,13 +3,19 @@ import { HttpClient } from '@angular/common/http';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Theater } from '../models/theater.model';
 import { Cinema } from '../models/cinema.model';
+import { Store } from '@ngxs/store';
+import { SetTheater } from '../ngxs/actions/theater.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TheaterService {
 
-  constructor(private http: HttpClient, private firestore: AngularFirestore) { }
+  constructor(private http: HttpClient, private firestore: AngularFirestore, private store: Store) { }
+
+  get(id: any) {
+    return this.firestore.collection('theaters').doc(id).valueChanges();
+  }
 
   async getAsync(id: any) {
     let theater = null;
@@ -17,6 +23,7 @@ export class TheaterService {
       theater = data.data() as Theater;
       theater.id = id;
     })
+    this.store.dispatch(new SetTheater(theater));
     return theater;
   }
 
@@ -34,7 +41,48 @@ export class TheaterService {
         })
       })
     })
+    this.store.dispatch(new SetTheater(theater));
     return theater;
+  }
+
+  async allWithAllCinemasAsync() {
+    const theaters: Theater[] = [];
+    await this.firestore.collection('theaters').ref.get().then(async docs => {
+      await docs.forEach(async doc => {
+        const theater = doc.data() as Theater;
+        theater.id = doc.id;
+        theater.cinemas = [];
+        await doc.ref.collection('cinemas').get().then(async items => {
+          await items.forEach(item => {
+            const cinema = item.data() as Cinema;
+            cinema.id = item.id;
+            theater.cinemas.push(cinema);
+          })
+        })
+        theaters.push(theater);
+      })
+    })
+    return theaters;
+  }
+
+  async AllActiveWithAllActiveCinemasAsync() {
+    const theaters: Theater[] = [];
+    await this.firestore.collection('theaters').ref.where('status', '==', true).get().then(async docs => {
+      await docs.forEach(async doc => {
+        const theater = doc.data() as Theater;
+        theater.id = doc.id;
+        theater.cinemas = [];
+        await this.firestore.collection('theaters').doc(doc.id).collection('cinemas').ref.where('status', '==', true).get().then(async items => {
+          await items.forEach(item => {
+            const cinema = item.data() as Cinema;
+            cinema.id = item.id;
+            theater.cinemas.push(cinema);
+          })
+        })
+        theaters.push(theater);
+      })
+    })
+    return theaters;
   }
 
   async allAsync() {
