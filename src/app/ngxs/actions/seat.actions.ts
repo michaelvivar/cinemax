455f8b280081af44fac86159c4eaf67e7284c603
Michael Vivar <michaelvivar@michaels-MacBook-Pro.local>
2018-06-26 15:58:34 +0800

    remove unused actions

--- src/app/ngxs/actions/seat.actions.ts
+++ src/app/ngxs/actions/seat.actions.ts
@@ -1,31 +1,17 @@
 import { Seat } from "../../models/seat.model";
 
-export class DisableSeat {
-    static readonly type = '[SEAT] Disabled';
-
-    constructor(public payload: Seat) {}
-}
-
-export class EnableSeat {
-    static readonly type = '[SEAT] Enabled';
-
-    constructor(public payload: Seat) {}
-}
-
-export class SetSeatValue {
-    static readonly type = '[SEAT] Set value';
-
-    constructor(public payload: { seat: Seat, value?: number }) {}
-}
-
-export class SetSeat {
-    static readonly type = '[SEAT] Set';
-
-    constructor(public payload: { [id: string]: Seat }) {}
-}
-
 export class AddSeat {
     static readonly type = '[SEAT] Add';
 
     constructor(public payload: Seat) {}
+}
+
+export class RemoveSeat {
+    static readonly type = '[SEAT] Remove';
+
+    constructor(public payload: Seat) {}
+}
+
+export class ResetSeat {
+    static readonly type = '[SEAT] Reset';
 }
