2bda14ea1aa896585a7dfe8d2d6a1b66db3e0acb
Michael Vivar <michaelvivar@michaels-MacBook-Pro.local>
2018-06-26 15:57:38 +0800

    added getAsync method

--- src/app/services/schedule.service.ts
+++ src/app/services/schedule.service.ts
@@ -8,13 +8,43 @@ import { combineLatest } from 'rxjs';
 import { Theater } from '../models/theater.model';
 import * as firebase from 'firebase/app';
 import { Movie } from '../models/movie.model';
+import { AUTOCOMPLETE_OPTION_HEIGHT } from '@angular/material';
+import { Store } from '@ngxs/store';
+import { SetTheater } from '../ngxs/actions/theater.actions';
+import { SetCinema } from '../ngxs/actions/cinema.actions';
+import { SetMovie } from '../ngxs/actions/movie.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScheduleService {
 
-  constructor(private firestore: AngularFirestore) { }
+  constructor(private firestore: AngularFirestore, private store: Store) { }
+
+  async getAsync(id: any) {
+    let schedule: Schedule;
+    await this.firestore.collection('schedules').doc(id).ref.get().then(async (sched) => {
+      schedule = sched.data() as Schedule;
+      schedule.date = new Date(schedule.date['seconds'] * 1000);
+      schedule.id = id;
+      await this.firestore.collection('movies').doc(schedule.movie).ref.get().then(movie => {
+        schedule.movie = movie.data();
+        schedule.movie.id = movie.id;
+      });
+      await this.firestore.collection('theaters').doc(schedule.theater).ref.get().then(theater => {
+        schedule.theater = theater.data();
+        schedule.theater.id = theater.id;
+      });
+      await this.firestore.collection('theaters').doc(schedule.theater.id).collection('cinemas').doc(schedule.cinema).ref.get().then(cinema => {
+        schedule.cinema = cinema.data();
+        schedule.cinema.id = cinema.id;
+      });
+    });
+    this.store.dispatch(new SetTheater(schedule.theater));
+    this.store.dispatch(new SetCinema(schedule.cinema));
+    this.store.dispatch(new SetMovie(schedule.movie));
+    return schedule;
+  }
 
   getByMovie(movieId: any) {
     return this.firestore.collection('movies').doc(movieId).snapshotChanges().pipe(map(doc => {
@@ -33,6 +63,16 @@ export class ScheduleService {
         })
       }))
     }))
+  }
+
+  getSeats(id: any) {
+    return this.firestore.collection('schedules').doc(id).collection('seats').snapshotChanges().pipe(map(docs => {
+      return docs.map(doc => {
+        const seat = doc.payload.doc.data() as Seat;
+        seat.id = doc.payload.doc.id;
+        return seat;
+      })
+    }));
   }
 
   all() {
@@ -78,12 +118,51 @@ export class ScheduleService {
     return this.firestore.collection('schedules').doc(id).delete();
   }
 
-  async publish(...id: string[]) {
+  async publish(...schedules: Schedule[]) {
     const batch = firebase.firestore().batch();
-    await id.forEach(o => {
-      const ref = this.firestore.collection('schedules').doc(o).ref;
+    schedules.forEach(async (sched) => {
+      const ref = this.firestore.collection('schedules').doc(sched.id).ref;
       batch.update(ref, { status: true });
     })
-    batch.commit();
+    await batch.commit();
+    this.addSeats(schedules);
+  }
+
+  private addSeats(schedules: Schedule[]) {
+    schedules.forEach(sched => {
+      this.firestore.collection('theaters').doc(sched.theater.id).collection('cinemas').doc(sched.cinema.id).collection('seats').ref.get().then(items => {
+        const seats = items.docs.map(item => item.data() as Seat);
+        let count = 0;
+        let batch = firebase.firestore().batch();
+        this.addSeatName(seats).sort((a, b) => a.row + b.row).forEach(seat => {
+          count++;
+          const seatRef = this.firestore.collection('schedules').doc(sched.id).collection('seats').doc(seat.row + '-' + seat.column).ref;
+          batch.set(seatRef, seat);
+          if (count == 50) {
+            batch.commit();
+            count = 0;
+            batch = firebase.firestore().batch();
+          }
+        });
+        batch.commit();
+      })
+    })
+  }
+
+  private addSeatName(seats: Seat[]) {
+    let x = 0;
+    for (let i = 1; i < 30; i++) {
+      let y = 1;
+      const seat = seats.filter(x => x.row == i && x.status == true).sort((a, b) => a.column - b.column);
+      if (seat.length > 0) {
+        seat.forEach(k => {
+          const index = seats.findIndex(o => o.row == k.row && o.column == k.column);
+          seats[index].name = letters[x] + '' + y;
+          y++;
+        })
+        x++;
+      }
+    }
+    return seats;
   }
 }
