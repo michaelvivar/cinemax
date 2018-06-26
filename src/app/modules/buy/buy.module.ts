cd785c00be5fac09293960bfd2f6acf8835fa169
Michael Vivar <michaelvivar@michaels-MacBook-Pro.local>
2018-06-26 15:59:35 +0800

    added components etc.

--- src/app/modules/buy/buy.module.ts
+++ src/app/modules/buy/buy.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BuyMovieComponent } from './buy-movie/buy-movie.component';
 import { RouterModule } from '@angular/router';
-import { MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatStepperModule, MatCheckboxModule, MatListModule, MatDividerModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
+import { BuyTicketComponent } from './buy-ticket/buy-ticket.component';
+import { ScheduleResolver } from '../../services/resolver/schedule.resolver';
+import { SeatLayoutComponent } from './seat-layout/seat-layout.component';
+import { PaymentFormComponent } from './payment-form/payment-form.component';
+import { UserModule } from '../../user/user.module';
 
-const materials = [MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule];
+const materials = [MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule,
+  MatCheckboxModule, MatButtonModule, MatStepperModule, MatListModule, MatDividerModule];
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule, ReactiveFormsModule, SharedModule,
+    CommonModule, FormsModule, ReactiveFormsModule, SharedModule, UserModule,
     RouterModule.forChild([
-      
+      { path: 'ticket/:id', component: BuyTicketComponent, resolve: { schedule: ScheduleResolver } }
     ]),
     ...materials
   ],
-  declarations: [BuyMovieComponent],
+  declarations: [BuyMovieComponent, BuyTicketComponent, SeatLayoutComponent, PaymentFormComponent],
 })
 export class BuyModule { }
