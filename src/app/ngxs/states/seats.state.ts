b212eaa1dc0eaf187f567a2eef755e034af2d97f
Michael Vivar <michaelvivar@michaels-MacBook-Pro.local>
2018-06-26 15:58:11 +0800

    removed unused actions

--- src/app/ngxs/states/seats.state.ts
+++ src/app/ngxs/states/seats.state.ts
@@ -1,51 +1,34 @@
 import { Seat } from "../../models/seat.model";
 import { State, Action, StateContext } from "@ngxs/store";
-import { SetSeatValue, EnableSeat, DisableSeat, SetSeat, AddSeat } from "../actions/seat.actions";
+import { AddSeat, RemoveSeat, ResetSeat } from "../actions/seat.actions";
 
 interface SeatStateModel {
-    [id: string]: Seat;
+    selected: Seat[];
 }
 
 @State<SeatStateModel>({
-    name: 'seats'
+    name: 'seats',
+    defaults: {
+        selected: []
+    }
 })
 export class SeatsState {
-
-    @Action(SetSeatValue)
-    setSeatValue(context: StateContext<SeatStateModel>, { payload }: SetSeatValue) {
-        const state = context.getState();
-        if (payload.value >= 0) {
-            payload.seat.value = payload.value;
-        }
-        state[payload.seat.id] = payload.seat;
-        context.setState(state);
-    }
-
-    @Action(EnableSeat)
-    enableSeat(context: StateContext<SeatStateModel>, { payload }: EnableSeat) {
-        const state = context.getState();
-        payload.status = true;
-        state[payload.id] = payload;
-        context.setState(state);
-    }
-
-    @Action(DisableSeat)
-    disableSeat(context: StateContext<SeatStateModel>, { payload }: DisableSeat) {
-        const state = context.getState();
-        payload.status = false;
-        state[payload.id] = payload;
-        context.setState(state);
-    }
-
-    @Action(SetSeat)
-    setSeat(context: StateContext<SeatStateModel>, { payload }: SetSeat) {
-        context.setState(payload);
-    }
 
     @Action(AddSeat)
     addSeat(context: StateContext<SeatStateModel>, { payload }: AddSeat) {
         const state = context.getState();
-        state[payload.id] = payload;
-        context.setState(state);
+        context.patchState({ selected: [...state.selected, payload] });
+    }
+
+    @Action(RemoveSeat)
+    removeSeat(context: StateContext<SeatStateModel>, { payload }: RemoveSeat) {
+        const state = context.getState();
+        const selected = state.selected.filter(o => !(o.row == payload.row && o.column == payload.column));
+        context.patchState({ selected });
+    }
+
+    @Action(ResetSeat)
+    resetSeat(context: StateContext<SeatStateModel>) {
+        context.setState({ selected : [] });
     }
 }
