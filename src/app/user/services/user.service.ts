d7e6e161aab6a9aac1718cb5fba7079ee19db7fa
Michael Vivar <michaelvivar@michaels-MacBook-Pro.local>
2018-06-26 15:56:00 +0800

    add user

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Injectable()
 export class UserService {
 
-  constructor(private _firebaseAuth: AngularFireAuth) { }
+  constructor(private _firebaseAuth: AngularFireAuth, private firestore: AngularFirestore) { }
 
   signInRegular(email, password) {
     //const credential = firebase.auth.EmailAuthProvider.credential( email, password );
@@ -15,13 +16,23 @@ export class UserService {
   }
 
   signUpRegular(email, password) {
-    return this._firebaseAuth.auth.createUserAndRetrieveDataWithEmailAndPassword(email, password);
+    return this._firebaseAuth.auth.createUserWithEmailAndPassword(email, password);
+    //return this._firebaseAuth.auth.createUserAndRetrieveDataWithEmailAndPassword(email, password);
   }
 
   signInWithTwitter() {
     return this._firebaseAuth.auth.signInWithPopup(
       new firebase.auth.TwitterAuthProvider()
     )
+  }
+
+  addUser(data) {
+    if (data.additionalUserInfo.isNewUser) {
+      this.firestore.collection('users').doc(data.user.uid).set({
+        provider: data.additionalUserInfo.providerId,
+        type: 'user'
+      })
+    }
   }
 
   signInWithFacebook() {
@@ -38,5 +49,9 @@ export class UserService {
 
   logout() {
     return this._firebaseAuth.auth.signOut();
+  }
+
+  get(id: any) {
+    return this.firestore.collection('users').doc(id).ref.get();
   }
 }
