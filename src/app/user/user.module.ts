6846562f6fe01ff8a155831174220058416ca188
Michael Vivar <michaelvivar@michaels-MacBook-Pro.local>
2018-06-26 15:57:14 +0800

    added components

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { UserService } from './services/user.service';
-import { MatFormFieldModule, MatInputModule, MatButtonModule, MatMenuModule } from '@angular/material';
+import { MatFormFieldModule, MatInputModule, MatButtonModule, MatMenuModule, MatDividerModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserButtonComponent } from './logout/user-button.component';
+import { UserMenuComponent } from './user-menu/user-menu.component';
+import { RouterModule } from '@angular/router';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { LoginButtonComponent } from './login-button/login-button.component';
+import { SignupButtonComponent } from './signup-button/signup-button.component';
 
-const materials = [MatFormFieldModule, MatInputModule, MatButtonModule, MatMenuModule];
+const materials = [MatFormFieldModule, MatInputModule, MatButtonModule, MatMenuModule, MatDividerModule];
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule, ReactiveFormsModule, ...materials
+    CommonModule, RouterModule, FormsModule, ReactiveFormsModule, ...materials
   ],
-  exports: [UserButtonComponent],
-  declarations: [LoginFormComponent, UserButtonComponent],
+  exports: [UserMenuComponent, LoginButtonComponent, SignupButtonComponent],
+  declarations: [LoginFormComponent, UserMenuComponent, SignupFormComponent, LoginButtonComponent, SignupButtonComponent],
   providers: [UserService],
-  entryComponents: [LoginFormComponent]
+  entryComponents: [LoginFormComponent, SignupFormComponent]
 })
 export class UserModule { }
