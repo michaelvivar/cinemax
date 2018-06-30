e16b66345db3d544c4eef04748a1c2d9c4bc59b6
Michael Vivar <michaelvivar@michaels-MacBook-Pro.local>
2018-07-01 07:19:12 +0800

    added safeUrl method

--- src/app/utils/html-helper.ts
+++ src/app/utils/html-helper.ts
@@ -5,3 +5,8 @@ export function safeHtml(html: string) {
     const domSanitizer: DomSanitizer = ServiceLocator.injector.get(DomSanitizer);
     return domSanitizer.bypassSecurityTrustHtml(html);
 }
+
+export function safeUrl(url: string) {
+    const domSanitizer: DomSanitizer = ServiceLocator.injector.get(DomSanitizer);
+    return domSanitizer.bypassSecurityTrustResourceUrl(url);
+}
