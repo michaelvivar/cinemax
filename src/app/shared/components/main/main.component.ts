2ff0d5386f0ce8c6935cb3dbfcba0f5adf4aa240
Michael Vivar <michaelvivar@michaels-MacBook-Pro.local>
2018-07-01 07:27:24 +0800

    added main(router-outlet)

---
+++ src/app/shared/components/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  template: '<router-outlet></router-outlet>'
+})
+export class Main implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
