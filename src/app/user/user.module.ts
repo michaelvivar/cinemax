95020beeaa075017b987578ea1c119d285a0a6cf
Michael Vivar <michaelvivar@michaels-MacBook-Pro.local>
2018-07-01 07:28:27 +0800

    added material modules

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { UserService } from './services/user.service';
-import { MatFormFieldModule, MatInputModule, MatButtonModule, MatMenuModule, MatDividerModule } from '@angular/material';
+import { MatFormFieldModule, MatInputModule, MatButtonModule, MatMenuModule, MatDividerModule, MatIconModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserMenuComponent } from './user-menu/user-menu.component';
 import { RouterModule } from '@angular/router';
@@ -10,7 +10,8 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
 import { LoginDialogDirective } from './directives/login-dialog.directive';
 import { SignupDialogDirective } from './directives/signup-dialog.directive';
 
-const materials = [MatFormFieldModule, MatInputModule, MatButtonModule, MatMenuModule, MatDividerModule];
+const materials = [MatFormFieldModule, MatInputModule, MatButtonModule, MatMenuModule, MatDividerModule,
+  MatIconModule];
 
 @NgModule({
   imports: [
